56e58152d5b4e5dc86ae795cd8967221a86fd7fc
rupadevii <rupadevi7094@gmail.com>
2025-09-26 14:00:20 +0530

    Minor Change

--- Blog/settings.py
+++ Blog/settings.py
@@ -226,7 +226,7 @@ STATICFILES_DIRS = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = BASE_DIR / 'media'
